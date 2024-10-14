9e04a775dcb89f7fc0f228fd58144dc97021110b
demigod <divyanshjain.2206@gmail.com>
2024-10-14 10:28:13 +0530

    optimise contract

--- src/app/api/contract/getContract.ts
+++ src/app/api/contract/getContract.ts
@@ -3,23 +3,19 @@ import { ethers, verifyMessage, Wallet } from "ethers";
 import contractABI from "./contractABI.json" assert { type: "json" };
 const contractAddress = "0xb41175aa3ff245c29BE0EC53AB37c8c319259daA";
 
-let contract: ethers.Contract | null = null;
+let Contract: ethers.Contract | null = null;
 
-export default async function GetContract() {
-    if (contract == null) {
-        const url = OasisSapphireTestnet.rpcUrls.public.http[0];
-        const privateKey = '0x' + process.env.PRIVATE_KEY;
-        // console.log(privateKey);
-        const signer = await new ethers.Wallet(privateKey);
-        const provider = await new ethers.JsonRpcProvider(url);
-        // console.log(signer);
-        const wallet = signer.connect(provider);
-        // console.log(wallet);
-        // const wallet = await new ethers.Wallet(privateKey)
-        console.log(signer.address);
-    
-        contract = new ethers.Contract(contractAddress, contractABI, wallet);
-    }
-
-    return contract;
+if (!process.env.PRIVATE_KEY) {
+    throw new Error('Invalid/Missing environment variable: "PRIVATE_KEY"')
 }
+if (Contract == null) {
+    const url = OasisSapphireTestnet.rpcUrls.public.http[0];
+    const privateKey = '0x' + process.env.PRIVATE_KEY;
+    const signer = await new ethers.Wallet(privateKey);
+    const provider = await new ethers.JsonRpcProvider(url);
+    const wallet = signer.connect(provider);
+    console.log(signer.address);
+    Contract = new ethers.Contract(contractAddress, contractABI, wallet);
+}
+
+export default Contract
--- src/app/api/contract/route.ts
+++ src/app/api/contract/route.ts
@@ -1,8 +1,7 @@
-import GetContract from "./getContract";
+import Contract from "./getContract";
 
 export async function GET(request: Request) {
-    const contract = await GetContract();
-    const balance = await contract.retrieve();
+    const balance = await Contract.retrieve();
     console.log("retrieve", balance.toString());
     return Response.json({balance: balance.toString()});
 }
@@ -10,13 +9,13 @@ export async function GET(request: Request) {
 export async function POST(request: Request) {
     const requestBody = await request.json();
     console.log(requestBody);
-    const contract = await GetContract();
+    // const contract = await Contract();
     const value = parseInt(requestBody.value);
     if (typeof value !== 'number') {
         console.log("NaN")
         return Response.json({error: "Value must be a number"}, {status: 400});
     }
-    await contract.store(requestBody.value);
+    await Contract.store(requestBody.value);
     console.log("Stored: ", requestBody.value);
     return Response.json({message: "POST request received"});
 }
